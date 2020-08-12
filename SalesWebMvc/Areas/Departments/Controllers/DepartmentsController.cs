88d334bf17189e6aed9839370f0ac493b90148fc
= <=>
2020-08-12 18:14:08 -0300

    Alterações na versão do .NET Core. Adicionando primeira área.

---
+++ SalesWebMvc/Areas/Departments/Controllers/DepartmentsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Areas.Departments.Models;
+
+namespace SalesWebMvc.Areas.Departments.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>()
+            {
+                new Department()
+                {
+                    Id = 1,
+                    Name = "Fashion"
+                },
+                new Department()
+                {
+                    Id = 2,
+                    Name = "Technology"
+                }
+            };
+            return View(departments);
+        }
+    }
+}
